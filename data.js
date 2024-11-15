28b686347b8d74d1dd2656a83b09b74e5b436ab0
Dath Analytics <dath.analyst@gmail.com>
2024-11-15 03:39:21 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,307 +1,195 @@
 export const bio = [
-  "Hello! I'm Vinay Somawat, an experienced Frontend Software Engineer with a proven ability to upgrade applications, optimize performance, and deliver innovative solutions.",
-  "I am passionate about using my technical skills to drive impactful contributions in challenging projects.",
+  "Hello! I'm Dimitris Athinaios, a Data Analytics and Data Science enthusiast pursuing my BSc in Business Management (Data Analytics and Digital Business) at Henley Business School, University of Reading.",
+  "I am passionate about uncovering insights and delivering impactful solutions through data-driven decision-making.",
   "Thanks for visiting!",
 ];
 
 export const skills = [
   {
     title: "Languages",
-    skillName: "JavaScript, TypeScript",
+    skillName: "Python, SQL",
     color: "1",
-    percentage: "80",
+    percentage: "90",
   },
   {
-    title: "Frameworks/Libraries",
-    skillName: "Angular, RxJS, NGRX/NGXS",
-    color: "6",
-    percentage: "70",
-  },
-  {
-    title: "Design",
-    skillName: "HTML, Bootstrap, Tailwind",
+    title: "Data Visualization Tools",
+    skillName: "Power BI, Tableau",
     color: "4",
-    percentage: "70",
-  },
-  {
-    title: "Version Control",
-    skillName: "GitHub, JIRA, Bitbucket",
-    color: "7",
-    percentage: "70",
-  },
-  {
-    title: "Tools",
-    skillName: "Debugging, Chrome DevTools",
-    color: "3",
     percentage: "80",
   },
-//   {
-//     title: "Analytics",
-//     skillName: "CleverTap, Countly",
-//     color: "5",
-//     percentage: "50",
-//   },
-//   {
-//     title: "IDE",
-//     skillName: "VS Code",
-//     color: "6",
-//     percentage: "70",
-//   },
+  {
+    title: "Data Analysis",
+    skillName: "Data Cleaning, Statistical Analysis",
+    color: "6",
+    percentage: "85",
+  },
+  {
+    title: "Software/Tools",
+    skillName: "Microsoft Office, Excel, R, Java",
+    color: "3",
+    percentage: "75",
+  },
+  {
+    title: "Development Tools",
+    skillName: "GitHub, R Commander",
+    color: "5",
+    percentage: "70",
+  },
 ];
 
 export const projects = {
   webProjects: [
     {
-      projectName: "Programming Diaries",
-      image: "images/programmingdiaries.png",
+      projectName: "Cargo Ship Project",
+      image: "images/cargo-ship.png",
       summary:
-        "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-      preview: "https://programmingdiaries.herokuapp.com/",
-      techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
+        "Analyzed tanker movements using AIS data to optimize shipping routes, improve fuel efficiency, assess port performance, and develop a predictive model for ETA.",
+      preview: "#",
+      techStack: ["Python", "SQL", "Power BI", "Pandas", "NumPy"],
     },
     {
-      projectName: "Find Your Bank",
-      image: "images/findyourbank.png",
+      projectName: "Churn Prediction Model",
+      image: "images/churn-prediction.png",
       summary:
-        "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-      preview: "https://clever-fermi-0d5d76.netlify.app",
-      techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+        "Built a churn prediction model to identify customers likely to leave, leveraging machine learning and advanced data analysis techniques.",
+      preview: "#",
+      techStack: ["Python", "Matplotlib", "Seaborn", "Scikit-learn"],
     },
     {
-      projectName: "Web Portfolio",
-      image: "images/portfolio.png",
+      projectName: "Amazon Web Scraping",
+      image: "images/web-scraping.png",
       summary:
-        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-      preview: "https://github.com/vinaysomawat/Travographer-Portal",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
+        "Developed a Python-based tool to scrape Amazon product data, track prices, and send alerts for price drops.",
+      preview: "#",
+      techStack: ["Python", "BeautifulSoup", "Requests", "Pandas"],
     },
     {
-      projectName: "Resume Builder",
-      image: "images/resume-builder.png",
+      projectName: "Data Cleaning Project SQL",
+      image: "images/data-cleaning.png",
       summary:
-        "Browser based editor to build and download Resumes in a customizable templates.",
-      preview: "https://vinaysomawat.github.io/Resume-Builder",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
-    },
-  ],
-  softwareProjects: [
-    {
-      projectName: "Pizza Ordering ChatBot",
-      image: "images/pizzaorderchatbot.png",
-      summary:
-        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
-      preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
-      techStack: ["Dailogflow", "Firebase"],
-    },
-    {
-      projectName: "WhatsApp-Bot",
-      image: "images/whatsappbot.jpg",
-      summary:
-        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-      preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
-      techStack: ["Selenium", "Chrome Webdriver", "Python"],
-    },
-    {
-      projectName: "Bill Generator",
-      image: "images/billgenerator.png",
-      summary:
-        "GUI to transfer data to excel sheets and generate bills on the local shops.",
-      preview: "https://github.com/vinaysomawat/Bill-Generator",
-      techStack: ["Tkinter", "Openxlpy", "Python"],
-    },
-  ],
-  androidProjects: [
-    {
-      projectName: "NITW-CSE",
-      image: "images/nitwcse.jpg",
-      summary:
-        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-      preview: "https://github.com/vinaysomawat/NITW-CSE",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-    {
-      projectName: "CareerHigh-App",
-      image: "images/carrerhigh.png",
-      summary:
-        "The Application display the webpages of website careerhigh.in in android devices.",
-      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-  ],
-  freelanceProjects: [
-    {
-      projectName: "SnylloAir.com",
-      image: "images/snylloair.png",
-      summary:
-        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-      preview: "https://www.snylloair.com/",
-      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-    },
-    {
-      projectName: "Delivery+",
-      image: "images/AM-Logo-.png",
-      summary: "Android Application to display website in android devices.",
-      preview:
-        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-      techStack: ["Android", "JAVA", "Play Store"],
+        "Cleaned and standardized datasets, removing duplicates and preparing data for analysis.",
+      preview: "#",
+      techStack: ["SQL", "Data Cleaning"],
     },
   ],
 };
 
 export const experience = [
   {
-    title: "Shiprocket (Bigfoot Solution Private Limited)",
-    duration: "September 2022 - Present",
-    subtitle: "Software Engineer",
-    details: ["Working in support and escalation team."],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "truck ",
+    title: "Globassure Insurance Agents (Howden)",
+    duration: "July 2024 - August 2024",
+    subtitle: "Data Analyst",
+    details: [
+      "Cleaned 15,000+ records and automated tasks with Python, saving 40 hours/month.",
+      "Leveraged Power BI insights to boost marketing ROI by 18%.",
+    ],
+    tags: ["Python", "Power BI", "Data Cleaning", "SQL"],
+    icon: "briefcase",
   },
   {
-    title: "Biofourmis India Private Limited",
-    duration: "April 2022 - Jul 2023",
-    subtitle: "Software Engineer",
+    title: "ICB, Athens",
+    duration: "June 2024 - July 2024",
+    subtitle: "Data Analyst",
     details: [
-      `Implemented microfrontends using the Module Federation Plugin in Angular.
-            Rendered components from remote modules/repositories, enabling a modular and scalable architecture`,
-      `Upgraded application with a dynamic base URL for flexible operation across multiple regions and verticals, leading to
-            recognition with the Biofourmis Bravo award in Q1, 2023.`,
-      `Implemented RxState for managing local state in components, resulting in elegant and reactive facades.`,
-      `Utilized Twilio-Video for group video call rooms and Countly for Web Analytics.`,
-      `Integrated NGXS WebSocket, enhancing state management and replacing legacy service injections.`,
-      `Implemented extensive RxJS usage for reactive programming, resulting in efficient data handling, improved state
-            management, and enhanced application performance.`,
-      `Dramatically optimized load time and performance through code splitting, lazy loading, caching, and preload`,
-      `Enhanced video call experience by implementing real-time switching of I/O devices and audio level indicators,
-            resulting in reduced audio issues and increased user satisfaction.`,
-      `Owned and led successful Angular application upgrades from version 12 to 13 and 14, improving performance
-            and enhancing features.`,
+      "Improved data accuracy by 15% by integrating SoftOne SQL data.",
+      "Created Power BI dashboards, reducing overstock by 25% and minimizing costs.",
     ],
-    tags: [
-      "JavaScript",
-      "Angular",
-      "RxJS",
-      "NGXS",
-      "TypeScript",
-      "RxState",
-      "Webpack",
-      "Optimization",
-    ],
-    icon: "heartbeat",
+    tags: ["Power BI", "SQL", "Data Visualization"],
+    icon: "chart-line",
   },
   {
-    title: "Novopay Solutions Private Limited",
-    duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
+    title: "Syndesis",
+    duration: "July 2023 - November 2023",
+    subtitle: "Co-Founder",
     details: [
-      `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
-            and Money transfer.`,
-      `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
-            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
-      `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
-      `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
+      "Conducted market research and analyzed insights using Excel & Power BI.",
+      "Led consultations with experts, expanding the advisory network by 80%.",
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "qrcode",
+    tags: ["Market Research", "Excel", "Power BI"],
+    icon: "user-tie",
   },
   {
-    title: "ThinkPedia LLP",
-    duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
+    title: "New Democracy Political Party",
+    duration: "May 2023 - July 2023",
+    subtitle: "Call Centre Agent",
     details: [
-      `Developed a customer web application for social media management, supporting the advertisement domain.`,
+      "Managed 350+ calls daily and influenced 10-50 prospective representatives.",
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-    icon: "group",
+    tags: ["Communication", "Customer Support"],
+    icon: "phone",
   },
 ];
 
 export const education = [
   {
-    title: "B.Tech. in Computer Science and Engineering",
-    duration: "2016 - 2020",
-    subtitle: "National Institute of Technology, Warangal",
-    details: [],
-    tags: [
-      "Data Structures & Algorithms",
-      "Operating Systems",
-      "Database Management System",
-      "Computer Networks",
-      "Compiler Designing",
-      "Cloud Computing",
+    title: "Henley Business School, University of Reading",
+    duration: "September 2022 - June 2025",
+    subtitle: "BSc in Business Management (Data Analytics and Digital Business)",
+    details: [
+      "Key Courses: Machine Learning, Data Analytics, Business Analytics.",
+      "Second Year Avg: 69% (2:1).",
     ],
+    tags: ["Machine Learning", "Data Analytics", "Business Analytics"],
     icon: "graduation-cap",
   },
   {
-    title: "Class 12th in Science and Mathematics",
-    duration: "",
-    subtitle: "Board of Secondary Education, Rajasthan",
-    details: [],
-    tags: ["Physics", "Chemistry", "Mathematics"],
-    icon: "book",
+    title: "St Catherine’s British School, Athens, Greece",
+    duration: "September 2017 - July 2021",
+    subtitle: "IB Diploma & IGCSEs",
+    details: [
+      "IB Diploma: HL - Mathematics AA, Physics, Business Management.",
+      "IGCSEs: 10 GCSEs including Mathematics(A) and English Language(A).",
+    ],
+    tags: ["Mathematics", "Physics", "Business Management"],
+    icon: "school",
+  },
+];
+
+export const certifications = [
+  {
+    title: "Google Advanced Data Analytics",
+    link: "#",
+  },
+  {
+    title: "Microsoft Power BI Desktop",
+    link: "#",
+  },
+  {
+    title: "IBM Data Analyst",
+    link: "#",
+  },
+  {
+    title: "Google Data Analytics",
+    link: "#",
+  },
+  {
+    title: "Data Analytics Bootcamp - Alex Freberg",
+    link: "#",
+  },
+  {
+    title: "Mathematics for Machine Learning Specialisation – Imperial College",
+    link: "#",
   },
 ];
 
 export const footer = [
-//   {
-//     label: "Dev Profiles",
-//     data: [
-//       {
-//         text: "Stackoverflow",
-//         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
-//       },
-//       {
-//         text: "GitHub",
-//         link: "https://github.com/vinaysomawat",
-//       },
-//       {
-//         text: "LeetCode",
-//         link: "https://leetcode.com/somawatvinay/",
-//       },
-//     ],
-//   },
-//   {
-//     label: "Resources",
-//     data: [
-//       {
-//         text: "Enable Dark/Light Mode",
-//         func: "enableDarkMode()",
-//       },
-//       {
-//         text: "Print this page",
-//         func: "window.print()",
-//       },
-//       {
-//         text: "Clone this page",
-//         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
-//       },
-//     ],
-//   },
   {
     label: "Links",
     data: [
-    //   {
-    //     text: "Linkedin",
-    //     link: "https://www.linkedin.com/in/vinaysomawat/",
-    //   },
-    //   {
-    //     text: "Twitter",
-    //     link: "https://twitter.com/thesigmakid",
-    //   },
       {
-        text: "Buy me a coffee",
-        link: "https://www.buymeacoffee.com/r194dME8y",
+        text: "LinkedIn",
+        link: "https://www.linkedin.com/in/dimitriosath/",
       },
       {
-        text: "Github/vinaysomawat.github.io",
-        link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+        text: "GitHub",
+        link: "https://github.com/daththeanalyst",
       },
     ],
   },
   {
     label: "copyright-text",
     data: [
-      "Made with &hearts; by Vinay Somawat.",
+      "Made with ♥ by Dimitris Athinaios.",
       "&copy; No Copyrights. Feel free to use this template.",
     ],
   },
