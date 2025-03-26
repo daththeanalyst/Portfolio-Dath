9c53f947779463f9e3a361eba646fcd3114f0076
Dath Analytics <dath.analyst@gmail.com>
2025-03-26 14:37:48 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
 export const bio = [
-  "Hello! I'm Dimitris Athinaios, a data analyst and data science enthusiast, currently completing my BSc in Business Management with Data Analytics and Digital Business at Henley Business School, University of Reading. I'm on track to graduate with a high 2:1 or First."
-  "This September, I’ll be starting my MSc in Business Analytics at UCL School of Management. I'm passionate about leveraging data to solve real-world problems and driven to build a career in data science."
+  "Hello! I'm Dimitris Athinaios, a data analyst and data science enthusiast, currently completing my BSc in Business Management with Data Analytics and Digital Business at Henley Business School, University of Reading. I'm on track to graduate with a high 2:1 or First. This September, I’ll be starting my MSc in Business Analytics at UCL School of Management. I'm passionate about leveraging data to solve real-world problems and driven to build a career in data science."
   "Thanks for visiting!"
 ];
 
